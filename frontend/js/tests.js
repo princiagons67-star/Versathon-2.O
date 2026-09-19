bf595edbd3c224b27c904b0be9723c0de55928dc
Yashwitha <yashwitha9902@gmail.com>
2026-09-19 13:08:16 +0530

    Add next question functionality

--- frontend/js/tests.js
+++ frontend/js/tests.js
@@ -1,3 +1,20 @@
+let currentQuestion = 0;
+
+const questions = [
+    {
+        question: "What is the output of 2 + 2?",
+        options: ["3", "4", "5", "6"]
+    },
+    {
+        question: "Which language is mainly used for web page structure?",
+        options: ["Python", "HTML", "C++", "Java"]
+    },
+    {
+        question: "Which data type is used to store True or False?",
+        options: ["String", "Boolean", "Integer", "Float"]
+    }
+];
+
 function selectDifficulty(level) {
     document.getElementById("selected-level").textContent =
         "You selected: " + level;
@@ -9,5 +26,22 @@ function selectAnswer(answer) {
 }
 
 function nextQuestion() {
-    alert("Next question will be displayed here.");
+
+    currentQuestion++;
+
+    if (currentQuestion >= questions.length) {
+        currentQuestion = 0;
+    }
+
+    document.getElementById("question").textContent =
+        questions[currentQuestion].question;
+
+    const optionButtons = document.querySelectorAll(".options button");
+
+    for (let i = 0; i < optionButtons.length; i++) {
+        optionButtons[i].textContent =
+            questions[currentQuestion].options[i];
+    }
+
+    document.getElementById("selected-answer").textContent = "";
 }
