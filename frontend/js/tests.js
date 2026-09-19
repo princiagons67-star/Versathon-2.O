634d9f451158909293e0e8c87ce625aa778a63d0
Yashwitha <yashwitha9902@gmail.com>
2026-09-19 13:06:08 +0530

    Add test questions and answer options

--- frontend/js/tests.js
+++ frontend/js/tests.js
@@ -2,3 +2,12 @@ function selectDifficulty(level) {
     document.getElementById("selected-level").textContent =
         "You selected: " + level;
 }
+
+function selectAnswer(answer) {
+    document.getElementById("selected-answer").textContent =
+        "You selected: " + answer;
+}
+
+function nextQuestion() {
+    alert("Next question will be displayed here.");
+}
